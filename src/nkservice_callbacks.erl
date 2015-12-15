1e911f20d6dafa5fa233e43674ef03e2fa8c212c
Carlos Gonzalez <carlosj.gf@gmail.com>
2015-12-15 20:16:50 +0100

    Minor updates

--- src/nkservice_callbacks.erl
+++ src/nkservice_callbacks.erl
@@ -24,15 +24,16 @@
 
 -export([service_init/2, service_handle_call/3, service_handle_cast/2, 
 		 service_handle_info/2, service_code_change/3, service_terminate/2]).
+-export_type([continue/0]).
+
 
 -type state() :: map().
--type nks_common() :: continue | {continue, list()}.
-
+-type continue() :: continue | {continue, list()}.
 
 
 %% @doc Called when a new service starts
 -spec service_init(nkservice:spec(), state()) ->
-	{ok, state()} | {stop, term()} | nks_common().
+	{ok, state()} | {stop, term()} | continue().
 
 service_init(_SrvSpec, State) ->
 	{ok, State}.
@@ -40,7 +41,7 @@ service_init(_SrvSpec, State) ->
 
 %% @doc Called when the service process receives a handle_call/3.
 -spec service_handle_call(term(), {pid(), reference()}, state()) ->
-	{reply, term(), state()} | {noreply, state()} | nks_common().
+	{reply, term(), state()} | {noreply, state()} | continue().
 
 service_handle_call(Msg, _From, State) ->
     lager:error("Module ~p received unexpected call ~p", [?MODULE, Msg]),
@@ -49,7 +50,7 @@ service_handle_call(Msg, _From, State) ->
 
 %% @doc Called when the NkApp process receives a handle_cast/3.
 -spec service_handle_cast(term(), state()) ->
-	{noreply, state()} | nks_common().
+	{noreply, state()} | continue().
 
 service_handle_cast(Msg, State) ->
     lager:error("Module ~p received unexpected cast ~p", [?MODULE, Msg]),
@@ -58,7 +59,7 @@ service_handle_cast(Msg, State) ->
 
 %% @doc Called when the NkApp process receives a handle_info/3.
 -spec service_handle_info(term(), state()) ->
-	{noreply, state()} | nks_common().
+	{noreply, state()} | continue().
 
 service_handle_info(Msg, State) ->
     lager:notice("Module ~p received unexpected info ~p", [?MODULE, Msg]),
@@ -66,7 +67,7 @@ service_handle_info(Msg, State) ->
 
 
 -spec service_code_change(term()|{down, term()}, state(), term()) ->
-    ok | {ok, state()} | {error, term()} | nks_common().
+    ok | {ok, state()} | {error, term()} | continue().
 
 service_code_change(OldVsn, State, Extra) ->
 	{continue, [OldVsn, State, Extra]}.
@@ -74,7 +75,7 @@ service_code_change(OldVsn, State, Extra) ->
 
 %% @doc Called when a service is stopped
 -spec service_terminate(term(), state()) ->
-	{ok, state()} | nks_common().
+	{ok, state()} | continue().
 
 service_terminate(_Reason, State) ->
 	{ok, State}.
